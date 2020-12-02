6628f133cef1496e034c7f4c5f041c713d2b23c0
Christopher Garay <cgaray@mitre.org>
2020-12-02 16:24:19 -0500

    use regex for pasword and policy parsing

--- python/day_2/run.py
+++ python/day_2/run.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python
 
+import pdb
+import re
 import sys
 from collections import Counter
+
+password_pattern = re.compile('(\d+)-(\d+)\s([a-z]):\s(\w+)')
 
 def read_passwords(input_file):
     passwords = list()
     with open(input_file, 'r') as f:
         for line in f.read().splitlines():
-            (policy_parts, password) = line.split(':')
-
-            range_parts = policy_parts.split(' ')[0]
-
-            policy = {
-                'min'      : int(range_parts.split('-')[0]),
-                'max'      : int(range_parts.split('-')[1]),
-                'character': policy_parts.split(' ')[1]
-            }
-
-            passwords.append(
-                {
-                    'policy'  : policy,
-                    'password': password.strip()
-                }
-            )
+            m = password_pattern.match(line)
+            passwords.append([int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)])
     return passwords
 
-def test_policy(policy, password):
-    c = Counter(password)
-    
-    return c[policy['character']] >= policy['min'] and c[policy['character']] <= policy['max']
+def test_policy(password, policy_type):
+    if policy_type == 'sled':
+        c = Counter(password[3])
+        test_result = c[password[2]] >= password[0] and c[password[2]] <= password[1]
+    elif policy_type == 'toboggan':
+        c = Counter([password[3][x-1] for x in (password[0], password[1])])
+        test_result = c[password[2]] == 1
+    else:
+        print(f"Unrecognized password policy {policy_type}")
+        sys.exit(1)
 
-def main(input_file):
+    return test_result
+
+def main(input_file, policy_type):
     test_results = list()
     for password in read_passwords(input_file):
-        test_results.append(test_policy(password['policy'], password['password']))
+        test_results.append(test_policy(password, policy_type))
     print(sum(test_results))
 
 if __name__ == '__main__':
     input_file = sys.argv[1]
+    policy_type = sys.argv[2]
     
-    main(input_file)
+    main(input_file, policy_type)
