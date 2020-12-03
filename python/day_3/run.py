109070c61ba942f7ff4c3cdc4ed17ed1f139766c
Christopher Garay <cgaray@mitre.org>
2020-12-03 08:23:33 -0500

    day 3 star 2

--- python/day_3/run.py
+++ python/day_3/run.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
 import sys
+from math import prod
 import pdb
 
 START_POS = (0, 0) # upper left position
-MOVE = (3, 1)      # right 3, down 1
+MOVES = [
+    (1, 1),
+    (3, 1),      # right 3, down 1
+    (5, 1),
+    (7, 1),
+    (1, 2)
+]
+
 
 def read_geography(filename):
     return open(filename, 'r').read().split()
@@ -24,13 +32,17 @@ def get_square(geography, pos):
 def main(input_file):
     geography = read_geography(input_file)
 
-    num_trees = 0
-    cur_pos = make_move(geography, START_POS, MOVE)
-    while cur_pos:
-        if get_square(geography, cur_pos) == '#':
-            num_trees += 1
-        cur_pos = make_move(geography, cur_pos, MOVE)
-    print(num_trees)
+    tree_counts = []
+    for move in MOVES:
+        num_trees = 0
+        cur_pos = make_move(geography, START_POS, move)
+        while cur_pos:
+            if get_square(geography, cur_pos) == '#':
+                num_trees += 1
+            cur_pos = make_move(geography, cur_pos, move)
+        tree_counts.append(num_trees)
+        print(f"Number of trees found for move {move}: {num_trees}.")
+    print(f"Product of tree counts: {prod(tree_counts)}")
 
 
 if __name__ == '__main__':
